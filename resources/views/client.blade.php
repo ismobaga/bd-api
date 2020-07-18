eb7a528bf749305e56e288dc7fe0d32e06344cbf
Abdoul wahab bagayoko <awahab.bagayoko@gmail.com>
2020-07-18 18:46:16 -0400

    deploy

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -2,24 +2,20 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-
-
+    <meta charset=utf-8>
+    <meta http-equiv=X-UA-Compatible content="IE=edge">
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <link rel=stylesheet type=text/css href=https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css> <link rel=stylesheet href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel=stylesheet href=https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css integrity=sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I crossorigin=anonymous>
+    <link rel=icon href=/favicon.ico> <title>client</title>
+    <link href=/css/app.a0a40826.css rel=preload as=style>
+    <link href=/js/app.0925e5fd.js rel=preload as=script>
+    <link href=/js/chunk-vendors.ac2975f1.js rel=preload as=script>
+    <link href=/css/app.a0a40826.css rel=stylesheet>
 </head>
 
-<body>
-
-    <div class="app">
-
-        <router-view></router-view>
-
-    </div>
-
-    <script src="/js/client.js"></script>
-</body>
-
-</html>
+<body><noscript><strong>We're sorry but client doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
+    <div id=app></div>
+    <script src=https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js integrity=sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo crossorigin=anonymous></script>
+    <script src=https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js integrity=sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/ crossorigin=anonymous></script>
+    <script src=/js/chunk-vendors.ac2975f1.js> </script> <script src=/js/app.0925e5fd.js> </script> </body> </html>
